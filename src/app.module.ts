ac4343834c01a2b96a13fd2b3d271c3a72f4f9c9
th-release <th.release.cth@gmail.com>
2023-02-01 21:08:35 +0900

    feat: import typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import 'dotenv'
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: 3306,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA,
+    entities: [],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
