6d656f907e71b2ba0add01ad2c94d6f0dc07e661
th-release <th.release.cth@gmail.com>
2023-02-01 22:50:08 +0900

    feat: Create cth module

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './repository/repository';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {};
