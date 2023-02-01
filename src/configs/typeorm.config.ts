6bff1198f32518179254eb0f7099b87271244270
th-release <th.release.cth@gmail.com>
2023-02-01 20:09:34 +0900

    chore(orm): add typeorm connection config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+import 'dotenv'
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: "mysql",
-  host: "127.0.0.1",
+  host: process.env.DATABASE_HOST,
   port: 3306,
-  username: "root",
-  password: "1234",
-  database: "yarn",
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_SCHEMA,
   entities: ['dist/**/*.entity.{ts,js}'],
-  synchronize: true //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
+  synchronize: true // true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
 }
