0119306a428acc702c20d5d85728db3faa58d9b7
th-release <cth.release@gmail.com>
2023-02-12 18:37:46 +0900

    remove: delete files

--- src/users/repository/repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { Users } from '../entity/users.entity'
-
-@EntityRepository(Users)
-export class UserRepository extends Repository<Users> {};
--- src/users/users.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service';
-
-@Controller('api/user')
-export class UsersController {
-  constructor(private readonly userService: UsersService) {};
-}
