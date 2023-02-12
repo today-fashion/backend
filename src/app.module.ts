05dbc21551415175db002dc800586d23738526d3
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-12 22:50:15 +0900

    feat: add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthEntity } from './auth/entity/auth.entity';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'today-fashion',
+      entities: [AuthEntity],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 
 export class AppModule {}
