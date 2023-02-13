03e334a5142545a7632f81d8b9f6ed03162dd7c1
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-13 19:40:02 +0900

    feat: Update auth.serivce

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { AuthEntity } from './entity/auth.entity';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Request, Response } from 'express'
+import { Repository } from 'typeorm'
+import { AuthEntity } from './entity/auth.entity'
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(AuthEntity)
     private authRepository: Repository<AuthEntity>
-  ) {};
-  
-  
+  ) {}
+
+  async naverCallback(req: Request, res: Response) {
+    
+  }
+
+  async googleCallback(req: Request, res: Response) {
+
+  }
+
+  async instagramCallback(req: Request, res: Response) {
+
+  }
+
+  async sendCode(req: Request, res: Response, type: string) {
+    
+  }
 }
