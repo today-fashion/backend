fa290c54d64dac1a7266988797e8737f4c5d2235
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-13 19:39:36 +0900

    feat: Update auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,30 @@
-import { AuthService } from './auth.service';
-import { AuthEntity } from './entity/auth.entity';
+import { Controller, Get, Param, Post, Put, Req, Res } from '@nestjs/common'
+import { Request, Response } from 'express'
+import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {};
   
+  @Get('/callbacks/:platform')
+  callbacks(@Req() req: Request, @Res() res: Response, @Param('platform') platform: string) {
+    switch(platform) {
+      case "naver":
+        this.authService.naverCallback(req, res);
+      case "google":
+        this.authService.googleCallback(req, res);
+      case "instagram":
+        this.authService.instagramCallback(req, res);
+      default:
+        return res.status(403).json({
+          message: "This platform is not supported.",
+          status: 403
+        })
+    }
+  }
+
+  @Put("/code/:type")
+  sendCode(@Req() req: Request, @Res() res: Response, @Param('type') type: string) {
+    this.authService.sendCode(req, res, type);
+  }
 }
