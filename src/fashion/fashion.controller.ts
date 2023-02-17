42073d72de1d4e078ff5c78ea8076598338306c5
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-17 09:56:15 +0900

    Delete src/fashion directory

--- src/fashion/fashion.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { FashionService } from './fashion.service';
-
-@Controller('fashion')
-export class FashionController {
-  constructor(private fashionService: FashionService) {};
-
-  
-}
