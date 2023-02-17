021f1617817e0a92f92110ff8a21b2bb89049db2
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-17 09:57:34 +0900

    feat: update entity file path

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthEntity } from './entity/auth.entity';
+import { AuthEntity } from '../entity/auth.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
